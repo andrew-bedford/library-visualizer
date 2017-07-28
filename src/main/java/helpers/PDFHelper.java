ad5dd8bbf657e1366d737d5c9dadc254882fadef
Andrew Bedford <andrew.bedford.1@ulaval.ca>
2017-07-28 17:28:12 -0400

    Added function that lists references (incomplete)

--- src/main/java/helpers/PDFHelper.java
+++ src/main/java/helpers/PDFHelper.java
@@ -5,6 +5,9 @@ import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class PDFHelper {
     public static String getText(File pdfFile) throws IOException {
@@ -12,5 +15,13 @@ public class PDFHelper {
         PDFTextStripper stripper = new PDFTextStripper();
         stripper.setParagraphEnd("\n");
         return stripper.getText(doc);
+    }
+
+    public static List<String> getReferences(File pdfFile) throws IOException {
+        List<String> references = new LinkedList<String>();
+        String text = getText(pdfFile);
+        String[] parts = text.split("(References|REFERENCES)[\\n]*\\[");
+
+        return Arrays.asList(parts[parts.length].split("\n\n"));
     }
 }
--- src/main/java/paperscout/Main.java
+++ src/main/java/paperscout/Main.java
@@ -6,13 +6,20 @@ import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String fileName = "C:\\Users\\Andrew Bedford\\OneDrive\\Library\\2016 - TaintART - A practical multi-level information-flow tracking system for android runtime.pdf";
         try {
             String text = PDFHelper.getText(new File(fileName));
-            System.out.println("Text in PDF: " + text);
+            //System.out.println("Text in PDF: " + text);
+
+            System.out.println("-------------------------------------------------------------");
+            List<String> references = PDFHelper.getReferences(new File(fileName));
+            for (String r : references) {
+                System.out.println("Reference : " + r.replace("\n", " "));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
